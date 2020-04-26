bb259566582f973b6ffb60f6ac76a25690d7461e
bcis93 <bryan.cisneros@gmail.com>
2020-04-26 12:02:09 -0600

    adding extern c statements to compile with c++

--- iot_sensor_gui/tmp102.h
+++ iot_sensor_gui/tmp102.h
@@ -1,9 +1,17 @@
 #ifndef TMP102_H__
 #define TMP102_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdbool.h>
 
 bool TMP102_Init(void);
 float TMP102_Read(void);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
 
 #endif /* TMP102_H__ */
