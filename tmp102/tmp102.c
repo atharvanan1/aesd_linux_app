c6e1ec89c285271660a563fedfb1adfbace42264
Atharva Nandanwar <atharva.nandanwar@colorado.edu>
2020-04-25 23:37:26 -0600

    Updated with comments

--- tmp102/tmp102.c
+++ tmp102/tmp102.c
@@ -3,6 +3,8 @@
  * File - tmp102.c
  * Purpose - implementation for tmp102 functions
  ***********************************************/
+
+/*********** Include Files ***********/
 #include "tmp102.h"
 #include <stdint.h>
 #include <stdbool.h>
@@ -18,13 +20,23 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/*********** Macro definitions ***********/
+// Slave address of TMP102
 #define TMP_102_ADDR    0x48
-#define I2C_DEVICE      "/dev/i2c-1"        // Connects to I2C5 peripheral
+// Connects to I2C5 peripheral, address 0x40015000
+#define I2C_DEVICE      "/dev/i2c-1"
+// Address to Temperature Register
 #define TEMP_REG        0x00
+// Address to Configuration Register
 #define CONFIG_REG      0x01
+// Macro for checking tmp_code MSB
 #define TMP_MSB         0x0800
+// Resolution of temperature sensor - used for calculations
 #define TMP_RES         0.0625f
+// Used for shifting MSB
+#define BIT_11         11
 
+/*********** Function Prototype ***********/
 /**
  * @brief TMP102_Init
  * Initializes TMP102 Sensor and sets up the pointer address to 
@@ -38,6 +50,7 @@ bool TMP102_Init(void)
     bool retval = false;
     bool file_open = false;
 
+    // Get the fd for I2C Device
     if(sensor_fd = open(I2C_DEVICE, O_RDWR) < 0) {
         syslog(LOG_ERR, "File Open: %s", strerror(errno));
         retval = true;
@@ -45,12 +58,14 @@ bool TMP102_Init(void)
     }
     file_open = true;
 
+    // Set it as slave
     if(ioctl(sensor_fd, I2C_SLAVE, TMP_102_ADDR) < 0) {
         syslog(LOG_ERR, "Error while initializing I2C Bus: %s", strerror(errno));
         retval = true;
         goto error_exit;
     }
 
+    // Write address of temperature register
     uint8_t init_value = TEMP_REG;
     if(write(sensor_fd, &init_value, 1)){
         syslog(LOG_ERR, "Error while writing: %s", strerror(errno));
@@ -58,6 +73,7 @@ bool TMP102_Init(void)
         goto error_exit;
     }
 
+    // Handle errors here
 error_exit:
     if(file_open) {
         close(sensor_fd);
@@ -76,22 +92,26 @@ float TMP102_Read(void)
     float temperature = 0;
     bool file_open = false;
 
+    // Get fd for I2C Device
     if(sensor_fd = open(I2C_DEVICE, O_RDWR) < 0) {
         syslog(LOG_ERR, "File Open: %s", strerror(errno));
     }
     file_open = true;
 
+    // Read from sensor
     uint8_t sensor_data[2] = {0 , 0};
     if(read(sensor_fd, sensor_data, 2) != 2) {
         syslog(LOG_ERR, "Error while initializing I2C Bus: %s", strerror(errno));
     }
 
+    // Close the file when operation over
     if(file_open) {
        close(sensor_fd);
     }
-
+    
+    // Algorithm for calculating temperature for digital data
     uint16_t tmp_code = sensor_data[1] << 4 + sensor_data[0] >> 4;
-    if(tmp_code & TMP_MSB) {
+    if((tmp_code & TMP_MSB >> BIT_11)) {
         tmp_code = ~(tmp_code) & 0x0FFF;
         tmp_code += 1;
         temperature = tmp_code * TMP_RES * -1;
